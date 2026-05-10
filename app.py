ba3681e4aeb44acecce64e2a77e7e4c085f46335
Chaithra <chaithragowda2110@gmail.com>
2026-05-10 14:49:01 +0530

    version 4.0

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def health():
 def info():
     return jsonify({
         "app": "DevOps Deployment Platform",
-        "version": "3.0",
+        "version": "4.0",
         "cloud": "AWS EC2",
         "container": platform.node()
     }), 200
